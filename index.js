9b5653f78795d17492df02fbeffa4c2e291fd486
eternaltreesnow <eternaltreesnow@gmail.com>
2017-04-10 22:24:11 +0800

    'modify'

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ let httpAgent = function(opts, draw, callback) {
 };
 
 let scheduleFunc = function() {
-    Schedule.scheduleJob('0 20 22 * * *', function() {
+    Schedule.scheduleJob('0 20 12 * * *', function() {
         console.log(new Date());
         updateRank(1);
     });
@@ -168,7 +168,37 @@ let getCode = function(date, type) {
 };
 
 let testSchedule = function() {
-    Schedule.scheduleJob('0 30 11 * * *', function() {
+    Schedule.scheduleJob('0 50 9 * * *', function() {
+        console.log(new Date());
+        updateRank(6);
+    });
+
+    Schedule.scheduleJob('0 10 10 * * *', function() {
+        console.log(new Date());
+        updateRank(6);
+    });
+
+    Schedule.scheduleJob('0 30 10 * * *', function() {
+        console.log(new Date());
+        updateRank(6);
+    });
+
+    Schedule.scheduleJob('0 50 10 * * *', function() {
+        console.log(new Date());
+        updateRank(6);
+    });
+
+    Schedule.scheduleJob('0 0 11 * * *', function() {
+        console.log(new Date());
+        updateRank(6);
+    });
+
+    Schedule.scheduleJob('0 20 11 * * *', function() {
+        console.log(new Date());
+        updateRank(6);
+    });
+
+    Schedule.scheduleJob('0 40 11 * * *', function() {
         console.log(new Date());
         updateRank(6);
     });
@@ -178,17 +208,22 @@ let testSchedule = function() {
         updateRank(6);
     });
 
+    Schedule.scheduleJob('0 20 12 * * *', function() {
+        console.log(new Date());
+        updateRank(6);
+    });
+
     Schedule.scheduleJob('0 30 12 * * *', function() {
         console.log(new Date());
         updateRank(6);
     });
 
-    Schedule.scheduleJob('0 0 13 * * *', function() {
+    Schedule.scheduleJob('0 20 13 * * *', function() {
         console.log(new Date());
         updateRank(6);
     });
 
-    Schedule.scheduleJob('0 30 13 * * *', function() {
+    Schedule.scheduleJob('0 40 13 * * *', function() {
         console.log(new Date());
         updateRank(6);
     });
@@ -198,7 +233,12 @@ let testSchedule = function() {
         updateRank(6);
     });
 
-    Schedule.scheduleJob('0 30 14 * * *', function() {
+    Schedule.scheduleJob('0 20 14 * * *', function() {
+        console.log(new Date());
+        updateRank(6);
+    });
+
+    Schedule.scheduleJob('0 40 14 * * *', function() {
         console.log(new Date());
         updateRank(6);
     });
@@ -207,38 +247,8 @@ let testSchedule = function() {
         console.log(new Date());
         updateRank(6);
     });
-
-    Schedule.scheduleJob('0 30 15 * * *', function() {
-        console.log(new Date());
-        updateRank(6);
-    });
-
-    Schedule.scheduleJob('0 0 16 * * *', function() {
-        console.log(new Date());
-        updateRank(6);
-    });
-
-    Schedule.scheduleJob('0 30 16 * * *', function() {
-        console.log(new Date());
-        updateRank(6);
-    });
-
-    Schedule.scheduleJob('0 0 17 * * *', function() {
-        console.log(new Date());
-        updateRank(6);
-    });
-
-    Schedule.scheduleJob('0 30 17 * * *', function() {
-        console.log(new Date());
-        updateRank(6);
-    });
-
-    Schedule.scheduleJob('0 0 18 * * *', function() {
-        console.log(new Date());
-        updateRank(6);
-    });
 };
 
-// testSchedule();
-scheduleFunc();
+testSchedule();
+// scheduleFunc();
 // updateRank(6);
